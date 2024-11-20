2e185dd813401f73e248ba9ea8457dbb26442ecf
Adeolu Abiodun <138899958+AdeoluAbiodun@users.noreply.github.com>
2024-11-20 10:55:01 -0500

    components php update

---
+++ components/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <nav aria-label="Footer Navigation">
+        <a href="https://github.com/adeoluabiodun/">GitHub</a> |
+        <a href="https://adeoluabiodun.github.io/">GitHub.io</a> |
+        <a href="https://www.codecademy.com/profiles/AdeoluAbiodun">Codecademy</a> |
+        <a href="https://www.freecodecamp.org/AdeoluAbiodun">FreeCodeCamp</a> |
+        <a href="https://jsfiddle.net/u/AdeoluAbiodun/fiddles/">JSFiddle</a> |
+        <a href="https://www.linkedin.com/in/adeolu-abiodun-42a858279/">LinkedIn</a>
+    </nav>
+    <br><br><p>Built by: <em>Adeolu T. Abiodun</em></p>
+</footer>
---
+++ components/header.php
@@ -0,0 +1,16 @@
+<header>
+    <div class="header-content">
+        <img src="images/logo.png" alt="Harmony Breeze Logo" class="logo">
+        <div>
+            <h1>Adeolu Abiodun's Harmony Breeze</h1>
+            <p>Bringing Rhythm to Every Moment</p>
+        </div>
+    </div>
+    <nav aria-label="Main Navigation">
+        <a href="index.php">Home</a> |
+        <a href="introduction.php">Introduction</a> |
+        <a href="contract.php">Contract</a> |
+        <a href="brand.php">Brand</a> |
+        <a href="introductionform2.php">Introduction Form</a>
+    </nav>
+</header>
